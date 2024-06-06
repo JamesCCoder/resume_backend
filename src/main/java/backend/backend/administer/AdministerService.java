b9ec0176d0272baff3e446757eb84548a3e9f748
James <justinwpro@gmail.com>
2024-06-06 08:24:51 -0500

    update

--- src/main/java/backend/backend/administer/AdministerService.java
+++ src/main/java/backend/backend/administer/AdministerService.java
@@ -1,15 +1,24 @@
 package backend.backend.administer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class AdministerService {
     @Autowired
     private AdministerRepository repository;
+//    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//    String hashedPassword = passwordEncoder.encode("123456");
 
     public boolean validateUser(String username, String password) {
         Administer administer = repository.findByUsername(username);
         return administer != null && administer.getPassword().equals(password);
     }
+
+//    public void saveAdminister(Administer administer) {
+//        administer.setPassword(passwordEncoder.encode(administer.getPassword()));
+//        repository.save(administer);
+//    }
 }
