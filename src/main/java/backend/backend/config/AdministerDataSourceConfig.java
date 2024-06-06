e116bc114e2f38ffb820b532d00a4904143ed9a2
James <justinwpro@gmail.com>
2024-06-06 13:37:34 -0500

    update

--- src/main/java/backend/backend/config/AdministerDataSourceConfig.java
+++ src/main/java/backend/backend/config/AdministerDataSourceConfig.java
@@ -1,57 +1,57 @@
-package backend.backend.config;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-@EnableJpaRepositories(
-        basePackages = "backend.backend.administer",
-        entityManagerFactoryRef = "administerEntityManagerFactory",
-        transactionManagerRef = "administerTransactionManager"
-)
-public class AdministerDataSourceConfig {
-
-    @Bean(name = "administerDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.administer")
-    public DataSource administerDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean(name = "administerEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean administerEntityManagerFactory(
-            @Qualifier("administerDataSource") DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan(new String[] { "backend.backend.administer" });
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        em.setJpaPropertyMap(properties);
-
-        return em;
-    }
-
-    @Bean(name = "administerTransactionManager")
-    public PlatformTransactionManager administerTransactionManager(
-            @Qualifier("administerEntityManagerFactory") LocalContainerEntityManagerFactoryBean administerEntityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(administerEntityManagerFactory.getObject());
-        return transactionManager;
-    }
-}
+//package backend.backend.config;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//@EnableJpaRepositories(
+//        basePackages = "backend.backend.administer",
+//        entityManagerFactoryRef = "administerEntityManagerFactory",
+//        transactionManagerRef = "administerTransactionManager"
+//)
+//public class AdministerDataSourceConfig {
+//
+//    @Bean(name = "administerDataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource.administer")
+//    public DataSource administerDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean(name = "administerEntityManagerFactory")
+//    public LocalContainerEntityManagerFactoryBean administerEntityManagerFactory(
+//            @Qualifier("administerDataSource") DataSource dataSource) {
+//        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//        em.setDataSource(dataSource);
+//        em.setPackagesToScan(new String[] { "backend.backend.administer" });
+//
+//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        em.setJpaVendorAdapter(vendorAdapter);
+//
+//        Map<String, Object> properties = new HashMap<>();
+//        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+//        em.setJpaPropertyMap(properties);
+//
+//        return em;
+//    }
+//
+//    @Bean(name = "administerTransactionManager")
+//    public PlatformTransactionManager administerTransactionManager(
+//            @Qualifier("administerEntityManagerFactory") LocalContainerEntityManagerFactoryBean administerEntityManagerFactory) {
+//        JpaTransactionManager transactionManager = new JpaTransactionManager();
+//        transactionManager.setEntityManagerFactory(administerEntityManagerFactory.getObject());
+//        return transactionManager;
+//    }
+//}
--- src/main/java/backend/backend/config/StudentsDataSourceConfig.java
+++ src/main/java/backend/backend/config/StudentsDataSourceConfig.java
@@ -1,59 +1,59 @@
-package backend.backend.config;
-
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-@EnableJpaRepositories(
-        basePackages = {"backend.backend.students", "backend.backend.professors"},
-        entityManagerFactoryRef = "studentsEntityManagerFactory",
-        transactionManagerRef = "studentsTransactionManager"
-)
-public class StudentsDataSourceConfig {
-
-    @Primary
-    @Bean(name = "studentsDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.students")
-    public DataSource studentsDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Primary
-    @Bean(name = "studentsEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean studentsEntityManagerFactory(
-            @Qualifier("studentsDataSource") DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("backend.backend.students", "backend.backend.professors");
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        em.setJpaPropertyMap(properties);
-
-        return em;
-    }
-
-    @Primary
-    @Bean(name = "studentsTransactionManager")
-    public PlatformTransactionManager studentsTransactionManager(
-            @Qualifier("studentsEntityManagerFactory") LocalContainerEntityManagerFactoryBean studentsEntityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(studentsEntityManagerFactory.getObject());
-        return transactionManager;
-    }
-}
+//package backend.backend.config;
+//
+//import javax.sql.DataSource;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//@EnableJpaRepositories(
+//        basePackages = {"backend.backend.students", "backend.backend.professors"},
+//        entityManagerFactoryRef = "studentsEntityManagerFactory",
+//        transactionManagerRef = "studentsTransactionManager"
+//)
+//public class StudentsDataSourceConfig {
+//
+//    @Primary
+//    @Bean(name = "studentsDataSource")
+//    @ConfigurationProperties(prefix = "spring.datasource.students")
+//    public DataSource studentsDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Primary
+//    @Bean(name = "studentsEntityManagerFactory")
+//    public LocalContainerEntityManagerFactoryBean studentsEntityManagerFactory(
+//            @Qualifier("studentsDataSource") DataSource dataSource) {
+//        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//        em.setDataSource(dataSource);
+//        em.setPackagesToScan("backend.backend.students", "backend.backend.professors");
+//
+//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        em.setJpaVendorAdapter(vendorAdapter);
+//
+//        Map<String, Object> properties = new HashMap<>();
+//        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+//        em.setJpaPropertyMap(properties);
+//
+//        return em;
+//    }
+//
+//    @Primary
+//    @Bean(name = "studentsTransactionManager")
+//    public PlatformTransactionManager studentsTransactionManager(
+//            @Qualifier("studentsEntityManagerFactory") LocalContainerEntityManagerFactoryBean studentsEntityManagerFactory) {
+//        JpaTransactionManager transactionManager = new JpaTransactionManager();
+//        transactionManager.setEntityManagerFactory(studentsEntityManagerFactory.getObject());
+//        return transactionManager;
+//    }
+//}
