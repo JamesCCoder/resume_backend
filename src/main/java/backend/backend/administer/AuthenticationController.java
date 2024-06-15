44a981f8d6fe0d170a517112d44cc9014cdfd57d
James <justinwpro@gmail.com>
2024-06-14 19:53:57 -0500

    update

--- src/main/java/backend/backend/administer/AuthenticationController.java
+++ src/main/java/backend/backend/administer/AuthenticationController.java
@@ -1,6 +1,6 @@
 package backend.backend.administer;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,23 +13,43 @@ import org.springframework.web.bind.annotation.*;
         "https://james-resume-backend-9a3094b7738e.herokuapp.com"
 })
 public class AuthenticationController {
+
     @Autowired
     private AdministerService administerService;
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody Administer administer) {
         if (administerService.findByUsername(administer.getUsername()) != null) {
-            return ResponseEntity.badRequest().body("Username is already taken!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is already taken!");
         }
         administerService.save(administer);
         return ResponseEntity.ok("User registered successfully");
     }
+
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody Administer administer) {
         boolean isValidUser = administerService.validateUser(administer.getUsername(), administer.getPassword());
         if (!isValidUser) {
-            return ResponseEntity.badRequest().body("Invalid username or password!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid username or password!");
         }
         return ResponseEntity.ok("Login successful");
     }
 
+    @RequestMapping(method = RequestMethod.OPTIONS, value = "/register")
+    public ResponseEntity<?> handleOptionsRegister() {
+        return ResponseEntity.ok()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With")
+                .build();
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS, value = "/login")
+    public ResponseEntity<?> handleOptionsLogin() {
+        return ResponseEntity.ok()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With")
+                .build();
+    }
 }
